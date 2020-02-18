47e05af1b2a1645983220831db4c2b3657b404ca
chrisVillanueva <chrisvillanuevanyc@gmail.com>
2020-02-18 18:27:48 -0500

    change: initial grid values

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,9 +1,9 @@
-export const MAX_ROWS = 15;
+export const MAX_ROWS = 30;
 export const MAX_COLS = 35;
 export const START_NODE_ROW = 3;
 export const START_NODE_COL = 5;
-export const FINISH_NODE_ROW = 5;
-export const FINISH_NODE_COL = 20;
+export const FINISH_NODE_ROW = 15;
+export const FINISH_NODE_COL = 25;
 
 
 
